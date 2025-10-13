c8adf29dc770bb2aa367a526d03341180d544b74
Terri-Lee-Dooling-FutureRent <terri-lee@futurerent.co.za>
2025-10-13 21:09:00 +0200

    added logo

--- beleza-store/src/components/Navbar.jsx
+++ beleza-store/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/beleza-logo.png";
 import "../styles/global.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="logo">Beleza</Link>
+      <Link to="/" className="logo">
+        <img src={logo} alt="BELEZA PROFESSIONAL PTY LTD" className="logo-image" />
+      </Link>
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
