1ba979502085cc16c7d967a7c21c724a4d924bcd
Terri-Lee-Dooling-FutureRent <terri-lee@futurerent.co.za>
2025-11-08 18:37:05 +0200

    added products

--- beleza-store/src/data/products.js
+++ beleza-store/src/data/products.js
@@ -1,27 +1,87 @@
 export const products = [
     {
       id: 1,
-      name: "Classic Lash Set",
-      category: "Lashes",
-      price: "R350",
-      image: new URL('../assets/products/Beauty/lashes1.png', import.meta.url).href,
+      name: "Cosmic Senses Wild Crafted Organic Sea Moss 500g",
+      category: "Sea Moss",
+      price: "R850",
+      image: new URL('../assets/products/Sea Moss/seamossorganic500g.png', import.meta.url).href,
 
-      description: "A timeless lash extension look for natural beauty."
+      //description: "500g Premium Wildcrafted Raw Seamoss is sustainably harvested from the pristine Caribbean waters and sundried to preserve its rich nutrient profile."
     },
     {
       id: 2,
-      name: "Silk Press",
+      name: "Creza Brazilian Cacau Treatment",
       category: "Hair",
-      price: "R450",
-      image: new URL('../assets/products/Hair/hair1.png', import.meta.url).href,
-      description: "Smooth, silky finish that lasts and shines."
+      price: "R1999",
+      image: new URL('../assets/products/Hair/crezabrazilliancacautreatment.png', import.meta.url).href,
+     // description: "Smooth, silky finish that lasts and shines."
     },
     {
       id: 3,
+      name: "Creza Collagen Botox Hair Treatment",
+      category: "Hair",
+      price: "R599",
+      image: new URL('../assets/products/Hair/crezacollagenbotoxhairtreatment.png', import.meta.url).href,
+      //description: "A calming treatment combining skincare and energy balance."
+    },
+    {
+      id: 4,
+      name: "Cosmic Senses Wild Crafted Organic Sea Moss 100g",
+      category: "Sea Moss",
+      price: "R380",
+      image: new URL('../assets/products/Sea Moss/seamossorganic100g.png', import.meta.url).href,
+
+      //description: "500g Premium Wildcrafted Raw Seamoss is sustainably harvested from the pristine Caribbean waters and sundried to preserve its rich nutrient profile."
+    },
+    {
+      id: 5,
+      name: "Cosmic Senses Elderberry Sea Moss 375ml",
+      category: "Sea Moss",
+      price: "R350",
+      image: new URL('../assets/products/Sea Moss/seamossgel.png', import.meta.url).href,
+
+      //description: "500g Premium Wildcrafted Raw Seamoss is sustainably harvested from the pristine Caribbean waters and sundried to preserve its rich nutrient profile."
+    },
+    {
+      id: 6,
+      name: "Cosmic Senses Sea Moss Rosehip Body Wash & Sea Moss Rosehip Creme",
+      category: "Sea Moss",
+      price: "R300",
+      image: new URL('../assets/products/Sea Moss/seamossbodywash.png', import.meta.url).href,
+
+      //description: "500g Premium Wildcrafted Raw Seamoss is sustainably harvested from the pristine Caribbean waters and sundried to preserve its rich nutrient profile."
+    },
+    {
+      id: 7,
+      name: "Cosmic Senses Blueberry Seamoss Bubble Bath Soak 1L",
+      category: "Sea Moss",
+      price: "R350",
+      image: new URL('../assets/products/Sea Moss/seamossbubblebath2.png', import.meta.url).href,
+
+      //description: "500g Premium Wildcrafted Raw Seamoss is sustainably harvested from the pristine Caribbean waters and sundried to preserve its rich nutrient profile."
+    },
+    {
+      id: 8,
+      name: "Capello Brazilian Keratin Treatment",
+      category: "Hair",
+      price: "R799",
+      image: new URL('../assets/products/Hair/capellobrazillian.png', import.meta.url).href,
+      //description: "Smooth, silky finish that lasts and shines."
+    },
+    {
+      id: 9,
       name: "Hair Mask",
       category: "Beauty",
       price: "R400",
       image: new URL('../assets/products/Hair/hair2.png', import.meta.url).href,
       description: "A calming treatment combining skincare and energy balance."
+    },
+    {
+      id: 10,
+      name: "Silk Press",
+      category: "Hair",
+      price: "R450",
+      image: new URL('../assets/products/Hair/hair1.png', import.meta.url).href,
+      description: "Smooth, silky finish that lasts and shines."
     }
-  ];
+]; 
